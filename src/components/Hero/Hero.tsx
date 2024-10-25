952cc214bc51407a9d74bfceb0d6c891f7fa2ed1
Marcos Carvalho <marcoscarvalhodev99@gmail.com>
2024-10-25 05:30:05 -0300

    feat: add gsap animations and nav bar

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import gsap from 'gsap';
+import styles from './Hero.module.css';
 
 const Hero = () => {
+  const welcomeRef = React.useRef<HTMLHeadingElement | null>(null);
+
+  {
+    /*React.useEffect(() => {
+    const ctx = gsap.context(() => {
+      function welcomeMove() {
+        gsap.to('.welcome-bg', {
+          translateX: '0rem',
+        });
+      }
+
+      function welcomeLeave() {
+        gsap.to('.welcome-bg', {
+          translateX: '-100%',
+        });
+      }
+
+      welcomeRef.current?.addEventListener('mouseenter', welcomeMove);
+      welcomeRef.current?.addEventListener('mouseleave', welcomeLeave);
+    });
+
+    ctx.revert();
+  });*/
+  }
+
   return (
-    <section className='absolute top-0 z-50 sm:w-[50rem] md:w-[80rem] p-[7.2rem]'>
-      <h1 className='sm:text-title-font-40 md:text-title-font-30'>
+    <section
+      className={`${styles.welcomeWrapper} absolute top-[50%] -translate-y-[50%] z-50 sm:w-[50rem] md:w-[80rem] p-[7.2rem]`}
+    >
+      <h1
+        ref={welcomeRef}
+        className={`${styles.welcomeText} welcome-text sm:text-title-font-40 md:text-title-font-30`}
+      >
         Welcome to my portfolio. I'm Marcos Carvalho, a software engineer with
         strong focus in development of dynamic 3D experiences.
       </h1>
+      <span
+        className={`${styles.welcomeBg} welcome-bg absolute w-[calc(100%+9.6rem)] h-[100%] bg-[#fcfeff] top-0 left-[-9.6rem] z-[-1] opacity-[1]`}
+      ></span>
     </section>
   );
 };
