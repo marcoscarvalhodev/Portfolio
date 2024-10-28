9b57fda3a13f64cc3fc9d1bcb15c3bdc1cf8e514
Marcos Carvalho <marcoscarvalhodev99@gmail.com>
2024-10-28 05:29:50 -0300

    feat: add smooth scroll to respective sections

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-
-gsap.registerPlugin(ScrollTrigger);
+import styles from './NavBar.module.css';
+import Logo from '../../assets/logo.svg?react';
+import ScrollSpy from '../../hooks/scrollSpy';
 
 const NavBar = () => {
+  const smoothRef = React.useRef<HTMLDivElement | null>(null);
+
   React.useEffect(() => {
     gsap.to('.header', {
       scrollTrigger: {
         trigger: 'body',
-        start: '100px',
+        start: '30px',
 
         onUpdate: (self) => {
           if (self.direction === 1) {
-            console.log('Scrolling down');
-            gsap.to('.header', { y: -150, duration: 1 });
+            gsap.to('.header', { y: -200, duration: 1 });
           } else {
-            console.log('Scrolling up');
             gsap.to('.header', { y: 0, duration: 1 });
           }
         },
@@ -28,33 +29,63 @@ const NavBar = () => {
     };
   }, []);
 
+  const { handleClick, activeSection } = ScrollSpy();
+
   return (
-    <header className='header fixed top-0 z-[999] left-[50%] -translate-x-[50%]'>
-      <nav className='flex gap-[2rem]'>
-        <a
-          href=''
-          className='text-[3.2rem] text-[#072a27] hover:text-[#24B3A8] transition-all duration-[0.5s]'
-        >
-          Home
-        </a>
-        <a
-          href='#projects'
-          className='text-[3.2rem] text-[#072a27] hover:text-[#24B3A8] transition-all duration-[0.5s]'
-        >
-          Projects
-        </a>
-        <a
-          href=''
-          className='text-[3.2rem] text-[#072a27] hover:text-[#24B3A8] transition-all duration-[0.5s]'
-        >
-          Technologies
-        </a>
-        <a
-          href=''
-          className='text-[3.2rem] text-[#072a27] hover:text-[#24B3A8] transition-all duration-[0.5s]'
-        >
-          Contact
-        </a>
+    <header className='header fixed top-0 z-[999] w-screen'>
+      <nav
+        className={`${styles.navBar} absolute right-0 flex rounded-b-[3rem] w-max mr-[4rem]`}
+      >
+        <div className='self-center p-[2rem] pr-0'>
+          <Logo className='w-[6rem] h-[6rem]' />
+        </div>
+
+        <div>
+          <div
+            ref={smoothRef}
+            className={`${styles.diagonalBg} bg-[#999da171] py-[1rem] m-[2rem] rounded-[3rem] overflow-hidden`}
+          >
+            <a
+              onClick={() => handleClick('section1')}
+              className={`${
+                activeSection === 'section1'
+                  ? 'bg-[#033980de] text-[#edf1f7de] '
+                  : ''
+              } cursor-pointer text-[1.8rem] text-[#091729] hover:bg-[#033980de] hover:text-[#edf1f7de] transition-all duration-[1s] p-[2rem]`}
+            >
+              Home
+            </a>
+
+            <a
+              onClick={() => handleClick('section2')}
+              className={`${
+                activeSection === 'section2' ? 'bg-[#033980de] text-[#edf1f7de]' : ''
+              } cursor-pointer text-[1.8rem] text-[#091729] hover:bg-[#033980de] hover:text-[#edf1f7de] transition-all duration-[1s] p-[2rem]`}
+            >
+              Projects
+            </a>
+
+            <a className='text-[1.8rem] text-[#091729] hover:bg-[#033980de] hover:text-[#edf1f7de] transition-all duration-[1s] p-[2rem]'>
+              Skills
+            </a>
+            <a
+              href=''
+              className='text-[1.8rem] text-[#091729] hover:bg-[#033980de] hover:text-[#edf1f7de] transition-all duration-[1s] p-[2rem]'
+            >
+              Contact
+            </a>
+          </div>
+          <div
+            className={`${styles.diagonalBg} bg-[#c59c06e3] m-[2rem] rounded-[3rem] overflow-hidden`}
+          >
+            <a
+              href=''
+              className='text-[1.8rem] text-[#091729] hover:bg-[#033980de] hover:text-[#edf1f7de] transition-all duration-[1s] text-center block p-[1rem]'
+            >
+              Want a more dimensional project?
+            </a>
+          </div>
+        </div>
       </nav>
     </header>
   );
